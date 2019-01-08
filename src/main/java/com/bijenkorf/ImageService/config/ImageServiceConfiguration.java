005f23374c6e7c2b6cfd2c0187ab2a248c6eb835
Adam Semenenko <adam.semenenko@gmail.com>
2019-01-08 17:03:59 +0100

    config for ignoring case when matching enums to Pathvariables

--- src/main/java/com/bijenkorf/ImageService/config/ImageServiceConfiguration.java
+++ src/main/java/com/bijenkorf/ImageService/config/ImageServiceConfiguration.java
@@ -1,10 +1,16 @@
 package com.bijenkorf.ImageService.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableConfigurationProperties
-public class ImageServiceConfiguration {
-
+public class ImageServiceConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        ApplicationConversionService.configure(registry);
+    }
 }
