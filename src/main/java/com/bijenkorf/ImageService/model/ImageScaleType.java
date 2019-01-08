5b06e6124dcb95b40556bec371a2f53099e2453f
Adam Semenenko <adam.semenenko@gmail.com>
2019-01-08 13:39:56 +0100

    Edit javadoc, remove duplication

--- src/main/java/com/bijenkorf/ImageService/model/ImageScaleType.java
+++ src/main/java/com/bijenkorf/ImageService/model/ImageScaleType.java
@@ -8,16 +8,16 @@ package com.bijenkorf.ImageService.model;
 public enum ImageScaleType {
 
 	/**
-	 * Crop, cut of parts of the image that no longer fit the new aspect ratio
+	 * Cut of parts of the image that no longer fit the new aspect ratio
 	 */
 	CROP,
 	/**
-	 * Fill, fill up the parts of the image that no longer fit the new aspect ration
+	 * Fill up the parts of the image that no longer fit the new aspect ration
 	 * with a background-color specified by the Fill-color property
 	 */
 	FILL,
 	/**
-	 * Skew, simply squeeze the image to fit the new height and width, this will
+	 * Simply squeeze the image to fit the new height and width, this will
 	 * make the image look bad in most cases
 	 */
 	SKEW
