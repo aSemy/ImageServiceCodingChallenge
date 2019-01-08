2be8216c176fdbfa29c8d9c5d1a8e7451ef79a39
Adam Semenenko <adam.semenenko@gmail.com>
2019-01-08 13:49:06 +0100

    flesh out the image storage service more

--- src/main/java/com/bijenkorf/ImageService/service/ImageStorageService.java
+++ src/main/java/com/bijenkorf/ImageService/service/ImageStorageService.java
@@ -1,8 +1,10 @@
 package com.bijenkorf.ImageService.service;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Optional;
 
 public interface ImageStorageService {
 	public String storeImage(String predefinedImageType, Optional<String> dummySeoNameOptional,
-			String relativeFileLocation_HTMLEscaped);
+			String relativeFileLocation_HTMLEscaped) throws MalformedURLException, IOException;
 }
--- src/main/java/com/bijenkorf/ImageService/service/ImageStorageServiceImpl.java
+++ src/main/java/com/bijenkorf/ImageService/service/ImageStorageServiceImpl.java
@@ -1,37 +1,114 @@
 package com.bijenkorf.ImageService.service;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Optional;
+
+import javax.imageio.ImageIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.HtmlUtils;
 
+import com.bijenkorf.ImageService.model.DefinedImageType;
 import com.bijenkorf.ImageService.properties.ImageServiceProperties;
 
 @Service
-public class ImageStorageServiceImpl  implements ImageStorageService {
+public class ImageStorageServiceImpl implements ImageStorageService {
 
 	@Autowired
 	private ImageServiceProperties imageServiceProperties;
+	@Autowired
+	private CloudHostingService cloudHostingService;
+	@Autowired
+	private ImageEditingService imageEditingService;
 
 	@Override
 	public String storeImage(String predefinedImageType, Optional<String> dummySeoNameOptional,
-			String relativeFileLocation_HTMLEscaped) {
-
-		final String relativeFileLocation = HtmlUtils.htmlUnescape(relativeFileLocation_HTMLEscaped);
-
-		// fetch image
-
-		final String imageToOptimise = imageServiceProperties.getSourceRootUrl() + relativeFileLocation;
-
-		// get image type
-
+			String relativeFileLocation_HTMLEscaped) throws MalformedURLException, IOException {
 		// ignore seo name for now
 
-		//
+		// set up variables
 
-		return null;
+		// convert escaped to normal
+		final String relativeFileLocation = HtmlUtils.htmlUnescape(relativeFileLocation_HTMLEscaped);
+		// get image type we want to convert the original to
+		DefinedImageType targetImageType = DefinedImageType.valueOf(predefinedImageType.toUpperCase());
+
+		////////////////
+		// core logic //
+		// \/\/\/\/\/\///
+
+		// check if optimised image is hosted
+		boolean isOptimisedHosted = cloudHostingService.imageExists(targetImageType, relativeFileLocation);
+
+		if (isOptimisedHosted) {
+			// Great! The optimised image is hosted
+			// We just have to fetch and return it
+
+			// TODO change return type to URL?
+			return cloudHostingService.getImageURL(targetImageType, relativeFileLocation).toString();
+		} else {
+			// the optimised image isn't hosted, so we need to
+			// - host the original (if necessary)
+			// - process the original
+			// - host the optimised image
+
+			// get the URL of the hosted original
+			// (by hosting it if necessary)
+			URL originalImageURL = storeOrFetchOriginalImageURL(relativeFileLocation);
+
+			// fetch the original
+			BufferedImage originalImage = ImageIO.read(originalImageURL);
+
+			// now process the original so it's optimised
+			Image processedImage = imageEditingService.processImage(originalImage, targetImageType);
+
+			// now store it
+			cloudHostingService.addImage(targetImageType, relativeFileLocation, processedImage);
+
+			// finally return it
+			return cloudHostingService.getImageURL(targetImageType, relativeFileLocation).toString();
+		}
 	}
 
+	/**
+	 * Get the URL of the original image. Either fetch it from the given URL and
+	 * store it, or fetch it from our cloud storage
+	 * 
+	 * @param relativeFileLocation
+	 * @return
+	 * @throws IOException
+	 */
+	private URL storeOrFetchOriginalImageURL(final String relativeFileLocation) throws IOException {
+
+		// check if image is stored in cloud
+		boolean isOriginalHosted = cloudHostingService.imageExists(DefinedImageType.ORIGINAL, relativeFileLocation);
+
+		if (!isOriginalHosted) {
+			// the original is NOT hosted, so store it
+
+			BufferedImage originalImage = ImageIO.read(getImageURL(relativeFileLocation));
+
+			cloudHostingService.addImage(DefinedImageType.ORIGINAL, relativeFileLocation, originalImage);
+			// confirm it now exists
+			// TODO throw a better exception
+			assert cloudHostingService.imageExists(DefinedImageType.ORIGINAL, relativeFileLocation);
+		}
+
+		// the original is now hosted, so return it
+		return cloudHostingService.getImageURL(DefinedImageType.ORIGINAL, relativeFileLocation);
+	}
+
+	private URL getImageURL(final String relativeFileLocation) throws MalformedURLException {
+		StringBuilder sb = new StringBuilder();
+		sb.append(imageServiceProperties.getSourceRootUrl());
+		sb.append(relativeFileLocation);
+
+		return new URL(sb.toString());
+	}
 
 }
